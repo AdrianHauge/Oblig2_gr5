1404f3eed523485dd27c098bdabfdd5bbf648d90
678291 <678291@stud.hvl.no>
2024-09-10 12:35:12 +0200

    Update main.java

--- Oppgave1/main.java
+++ Oppgave1/main.java
@@ -1,9 +1,15 @@
+import javax.swing.JOptionPane;
+
 public class main {
 
 public static void main(String[] args){
 
+int valg = JOptionPane.showConfirmDialog(null, "Skriv inn din mel ding, quit for og slutte", "Homo?",JOptionPane.YES_NO_CANCEL_OPTION);
 
 
 }
 
+
 }
+
+
