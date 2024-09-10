363309612f1bfb405bc394d0804a4c035421464b
H673498 Nanthawat <nanthawatbutsri1999@gmail.com>
2024-09-10 14:26:16 +0200

    Update HamburgerBrett.java

--- Oppgave2/HamburgerBrett.java
+++ Oppgave2/HamburgerBrett.java
@@ -1,23 +1,39 @@
 package Oppgave2;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class HamburgerBrett {
 
+    private Queue<Hamburger> brett;
+    private int størrelse;
+    private int teller;
+
+    public HamburgerBrett(int størrelse) {
+        this.størrelse = størrelse;
+        this.teller = 0;
+        this.brett = new LinkedList<Hamburger>();
+    }
+
     public boolean erFullt() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'erFullt'");
+        return størrelse == teller;
+    }
+
+    public boolean erTom() {
+        return teller == 0;
     }
 
     public void leggTilHamburger(String navn) {
         Hamburger burger = new Hamburger();
         teller++;
         brett.add(burger);
+
         System.out.println(navn + " (kokk) legger på hamburger "+ burger.toString() + ". Brett: " + brett.toString());
     }
 
     public void taHamburger(String navn) {
+
         Hamburger burger = brett.poll();
         teller--;
         System.out.println(navn + " (servitør) tar av hamburger" + burger.toString() + ". Brett: " + brett.toString());
     }
-
-}
